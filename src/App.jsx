9d7d71b8e045d7f9d322fee2dd9f15a9118b431f
IvanR33 <ivan.jimenez13@inacapmail.cl>
2026-06-14 04:24:01 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Cronología de Hardware</h1>
+      <p>La aplicación está cargando correctamente.</p>
+    </div>
+  );
+}
+
+export default App;
